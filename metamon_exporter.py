23fdaeac540a0d0b64d44f0c9455147766960964
Alexander Kondratyev <a.kondratev@city-mobil.ru>
2021-12-09 01:25:28 +0300

    add eggs and potions

--- metamon_exporter.py
+++ metamon_exporter.py
@@ -10,11 +10,9 @@ import aiohttp
 # Create a metric to track time spent and requests made.
 REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
 METAMON_PRICE = Gauge('metamon_price', 'Info about METAMON on market', ['id', 'sale_address', 'level'])
+EGGS_PRICE = Gauge('eggs_price', 'Info about EGGS on market', ['id', 'sale_address'])
+POTIONS_PRICE = Gauge('potions_price', 'Info about POTIONS on market', ['id', 'sale_address'])
 
-# &category=17
-# 15 - potions
-# 13 - mtm
-# 17 - potions
 base_url = 'https://market-api.radiocaca.com/nft-sales?pageNo=1&pageSize=20&sortBy=single_price&order=asc&name=&saleType&tokenType&tokenId=-1'
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:94.0) Gecko/20100101 Firefox/94.0',
            'Accept': '*/*', 'Accept-Language': 'en-US,en;q=0.5', 'Referer': 'https://market.radiocaca.com/',
@@ -48,11 +46,29 @@ def metamon_request(base_url, headers):
     asyncio.run(metamon_info(url_list))
 
 
+def eggs_request(base_url, headers):
+    url = base_url + '&category=17'
+    eggs_price_response = requests.get(url, headers=headers)
+    eggs_price_json = json.loads(eggs_price_response.text)
+    for el in eggs_price_json['list']:
+        EGGS_PRICE.labels(id=el['id'], sale_address=el['sale_address']).set(el['fixed_price'])
+
+
+def potions_request(base_url, headers):
+    url = base_url + '&category=15'
+    potions_price_response = requests.get(url, headers=headers)
+    potions_price_json = json.loads(potions_price_response.text)
+    for el in potions_price_json['list']:
+        POTIONS_PRICE.labels(id=el['id'], sale_address=el['sale_address']).set(el['fixed_price'])
+
+
 if __name__ == '__main__':
     start_http_server(8000)
     while True:
         start = time.time()
         metamon_request(base_url, headers)
+        eggs_request(base_url, headers)
+        potions_request(base_url, headers)
         end = time.time()
-        print("Took {} seconds to pull 20 websites.".format(end - start))
+        print("Took {} seconds to pull metrics.".format(end - start))
         time.sleep(5)
